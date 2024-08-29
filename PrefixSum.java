8c440596a073c945d19ff17dbaf4d2f3f55cd56a
shivam240104 <mauryatanmay845@gmail.com>
2024-08-29 07:30:21 +0530

    prefix max Subarray

---
+++ PrefixSum.java
@@ -0,0 +1,32 @@
+public class PrefixSum {
+    public static void prefix(int num[]){
+        int currSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        int prefix[]=new int [num.length];
+
+        prefix[0]=num[0];
+
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+num[i];
+        }
+        for(int i=0;i<num.length;i++){
+            int start=i;
+            for(int j=i;j<num.length;j++){
+                int end=j;
+                currSum=start==0?prefix[end]:prefix[end]-prefix[start-1];
+
+                if(maxSum<currSum){
+                    maxSum=currSum;
+
+                }
+            }
+        }
+        System.out.println("max sum =" +maxSum);
+    }
+
+    public static void main(String[] args) {
+        int num[]={2,4,6,8,9};
+        prefix(num);
+    }
+    
+}
